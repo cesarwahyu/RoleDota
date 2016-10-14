228ae876fc674b84a65dedad010dc0b4579ffded
Cesar Wahyu Cahya A <cesar.wahyu10@gmail.com>
2016-10-15 02:39:44 +0700

    My New Design

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4010/roledota/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4010/roledota/MainActivity.java
@@ -2,12 +2,56 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl4010.roledota;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText etNama;
+    Button bOk;
+    RadioButton rbR, rbD;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        bOk.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                doProses();
+                doClick();
+            }
+        });
+    }
+
+    private void doClick()
+    {
+
+    }
+
+    private void doProses()
+    {
+        String nama = etNama.getText().toString();
+
+        rbR = (RadioButton) findViewById(R.id.radioButtonR);
+        rbD = (RadioButton) findViewById(R.id.radioButtonD);
+
+        tvHasil.setText(nama);
     }
 }
